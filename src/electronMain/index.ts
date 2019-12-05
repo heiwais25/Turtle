4755e2057fc01b64590cba40b1b8c50d460a09a0
heiwais25 <heiwais25@gmail.com>
2019-12-05 23:36:59 +0900

    Add auto updater

--- src/electronMain/index.ts
+++ src/electronMain/index.ts
@@ -1,4 +1,6 @@
 import electron, { app, BrowserWindow, ipcMain } from "electron";
+import { autoUpdater } from "electron-updater";
+import log from "electron-log";
 import path from "path";
 import DatabaseService from "./db";
 import installExtensions, {
@@ -6,6 +8,9 @@ import installExtensions, {
   REDUX_DEVTOOLS
 } from "electron-devtools-installer";
 import "electron-reload";
+
+log.transports.file.level = "info";
+autoUpdater.logger = log;
 
 let mainWindow: electron.BrowserWindow | null;
 function createWindow() {
@@ -43,7 +48,9 @@ app.on("will-quit", () => {
   (global as any).db = null;
 });
 
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+});
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -54,4 +61,45 @@ app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+// ================================================================================
+// Manage auto update related functions
+function sendStatusToWindow(text: string) {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send("message", text);
+  }
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", info => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", info => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", err => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", progressObj => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", info => {
+  sendStatusToWindow("Update downloaded");
+});
+
+app.on("ready", function() {
+  autoUpdater.checkForUpdatesAndNotify();
 });
