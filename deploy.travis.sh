e777f95971aea911a79be720d6b72793722d1156
heiwais25 <heiwais25@gmail.com>
2019-12-05 20:00:11 +0900

    Change the script to publish
    - Change the version tag
    - Change the script to include publish flag

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn build
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:12-11.19 /bin/bash -c "yarn install && yarn release"
+    docker run --rm -e GH_TOKEN=$GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:12-11.19 /bin/bash -c "yarn install && yarn release"
 fi
